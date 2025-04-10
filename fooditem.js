789cc133505776e1593750facde6fbb115d42521
karimmohdrafi <mohdrafi0701@gmail.com>
2025-04-10 11:08:48 +0530

    web

---
+++ fooditem.js
@@ -0,0 +1,935 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Dum Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 170,
+    img: 'dum.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Kabab Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 200,
+    img: 'Kabab.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 100,
+    img: 'egg.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Lotus Special chicken',
+    category : 'biryani',
+    rating : 4.3,
+    price: 150,
+    img: 'Lotus.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Fry Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 180,
+    img: 'fry.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Joint Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 240,
+    img: 'joint.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 220,
+    img: 'mu.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Rambo Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 200,
+    img: 'rambo.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Nattu Kodi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 240,
+    img: 'Natu.jpg',
+    quantity: 1
+},
+
+{
+    id: 10,
+    name: 'Mutton Dum',
+    category : 'biryani',
+    rating : 4.3,
+    price: 300,
+    img: 'mutton dum.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Mutton Mughlai',
+    category : 'biryani',
+    rating : 4.3,
+    price: 320,
+    img: 'mug.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Mutton kheema',
+    category : 'biryani',
+    rating : 4.3,
+    price: 300,
+    img: 'kheema.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Fish Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 180,
+    img: 'fish.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Prawns Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 200,
+    img: 'Prawns.jpg',
+    quantity: 1
+},
+
+{
+    id: 15,
+    name: 'Kamju Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 250,
+    img: 'kamju.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Family Pack chicken',
+    category : 'biryani',
+    rating : 4.3,
+    price: 450,
+    img: 'family.jpg',
+    quantity: 1
+},
+
+{
+    id: 13,
+    name: 'Butter Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 210,
+    img: 'butter chicken.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Kadai Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 210,
+    img: 'kadai.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Chicken Rayalaseema',
+    category : 'chicken',
+    rating : 4.3,
+    price: 210,
+    img: 'Raya.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Punjabi Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 210,
+    img: 'punjabi.jpg',
+    quantity: 1
+},
+{
+    id: 17,
+    name: 'Chicken Afghani',
+    category : 'chicken',
+    rating : 4.3,
+    price: 220,
+    img: 'chicken.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Telagana Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 190,
+    img: 'tel.jpg',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'Lotus Special',
+    category : 'chicken',
+    rating : 4.3,
+    price: 220,
+    img: 'lotus curry.jpg',
+    quantity: 1
+},
+{
+    id: 20,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 180,
+    img: 'curry.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Mutton masala',
+    category : 'chicken',
+    rating : 4.3,
+    price: 250,
+    img: 'goat.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Mutton Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 220,
+    img: 'mutton curry.jpg',
+    quantity: 1
+},
+
+{
+    id: 23,
+    name: 'Mutton Mughlai',
+    category : 'chicken',
+    rating : 4.3,
+    price: 260,
+    img: 'mutton mug curry.jpg',
+    quantity: 1
+},
+
+{
+    id: 24,
+    name: 'Prawns Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 200,
+    img: 'prawns curry.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Kadai Prawn',
+    category : 'chicken',
+    rating : 4.3,
+    price: 200,
+    img: 'kadai prawn curry.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Prawns Kolhapuri',
+    category : 'chicken',
+    rating : 4.3,
+    price: 200,
+    img: 'kolhapuri prawn.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Fish Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 180,
+    img: 'fish curry.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Fish Kadai',
+    category : 'chicken',
+    rating : 4.3,
+    price: 180,
+    img: 'kadai fish.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Mutton KolhaPuri',
+    category : 'Chicken',
+    rating : 4.3,
+    price: 250,
+    img: 'images/paneer/Matar-Paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 30,
+    name: 'Chicken 65',
+    category : 'paneer',
+    rating : 4.3,
+    price: 190,
+    img: 'Chicken 65.jpg',
+    quantity: 1
+
+},
+{
+    id: 31,
+    name: 'chicken chilly',
+    category : 'paneer',
+    rating : 4.3,
+    price: 190,
+    img: 'Chicken chilli.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chicken Manchuria',
+    category : 'paneer',
+    rating : 4.3,
+    price: 190,
+    img: 'Chicken Manchuria.jpg',
+    quantity: 1
+
+},
+{
+    id: 34,
+    name: 'Lollipop chicken(4 pcs)',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'Lollipop.jpg',
+    quantity: 1
+
+},
+{
+    id: 35,
+    name: 'Chicken Drumsticks(4pcs)',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'drum.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Chicken Wings',
+    category : 'paneer',
+    rating : 4.3,
+    price: 190,
+    img: 'chicken wings.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Khaju Nut Chicken',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'khaju nut.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Mutton 65',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 230,
+    img: 'mutton 65.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Mutton Chilly',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 230,
+    img: 'mutton chilli.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mutton Manchuria',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 230,
+    img: 'mutton manchuria.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Prawns 65',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 230,
+    img: 'prawn 65.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Fish 65',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 180,
+    img: 'fish 65.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Schezwan Chicken dry',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 190,
+    img: 'schz.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'RajaDhani Chicken',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'raja.jpg',
+    quantity: 1
+},
+{
+    id: 45,
+    name: 'Hong Hong Chicken Dry',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'hong.jpg',
+    quantity: 1
+},
+{
+    id: 46,
+    name: 'Andhra Chicken',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'andhra.jpg',
+    quantity: 1
+},
+
+{
+    id: 42,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 180,
+    img: 'Veg Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Paneer Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 250,
+    img: 'Paneer Biryani.jpg',
+    quantity: 1
+},
+
+{
+    id: 30,
+    name: 'Mushroom Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 170,
+    img: 'Mushroom biryani.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Baby Corn Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 170,
+    img: 'baby corn.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'khaju Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 200,
+    img: 'khaju.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Veg Fried Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 150,
+    img: 'veg fried.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Paneer Fried Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 150,
+    img: 'paneer.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Mushroom Fried Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 150,
+    img: 'Mushroom fried rice.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Baby corn Fried Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 150,
+    img: 'corn.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Khaju Fried Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 180,
+    img: 'khaju fried rice.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Curd Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 80,
+    img: 'curd.jpg',
+    quantity: 1
+},
+
+{
+    id: 29,
+    name: 'Schezwan Fried Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 150,
+    img: 'schz fried.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Ghee Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 170,
+    img: 'ghee.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Jeera Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 100,
+    img: 'jeera.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Tomato Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 100,
+    img: 'tomato.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Lemon Rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 100,
+    img: 'Lemon.jpg',
+    quantity: 1
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+    id: 31,
+    name: 'Paneer 65',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'paneer.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Paneer Manchuria',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'paneer Manchuria.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Paneer hilly',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'paneer chilli.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Mushroom 65',
+    category : 'chinese',
+    rating : 4.3,
+    price: 170,
+    img: 'Mushroom 65.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Mushroom chilly',
+    category : 'chinese',
+    rating : 4.3,
+    price: 170,
+    img: 'mushroom chilli.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Mushroom Manchuria',
+    category : 'chinese',
+    rating : 4.3,
+    price: 170,
+    img: 'mushroom manchuria.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Veg 65',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'Veg 65.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Veg Manchuria',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'veg manchuria.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Veg Chilly',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'veg chilli.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Baby Corn Chilly',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'baby corn chilli.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Baby Corn 65',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'baby corn 65.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Baby corn Manchuria',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'baby corn manchuria.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Khaju Roast',
+    category : 'chinese',
+    rating : 4.3,
+    price: 200,
+    img: 'khaju roast.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Veg Banjara',
+    category : 'chinese',
+    rating : 4.3,
+    price: 200,
+    img: 'Veg banjara.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Veg Boorani',
+    category : 'chinese',
+    rating : 4.3,
+    price: 200,
+    img: 'veg boorani.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'French Fries(Full)',
+    category : 'chinese',
+    rating : 4.3,
+    price: 160,
+    img: 'french fries.jpg',
+    quantity: 1
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+    id: 37,
+    name: 'Paneer Butter Masala',
+    category : 'south indian',
+    rating : 4.3,
+    price: 170,
+    img: 'butter masala.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Kadai Paneer',
+    category : 'south indian',
+    rating : 4.3,
+    price: 170,
+    img: 'paneer curry.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Mushroom curry',
+    category : 'south indian',
+    rating : 4.3,
+    price: 180,
+    img: 'mushroom curry.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Khaju paneer',
+    category : 'south indian',
+    rating : 4.3,
+    price: 190,
+    img: 'Khaju paneer curry.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Methi Chaman',
+    category : 'south indian',
+    rating : 4.3,
+    price: 160,
+    img: 'methi.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Aalu Gobi curry',
+    category : 'south indian',
+    rating : 4.3,
+    price: 180,
+    img: 'alu.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Mix Veg Curry',
+    category : 'south indian',
+    rating : 4.3,
+    price: 180,
+    img: 'mix.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Veg Chatphata',
+    category : 'south indian',
+    rating : 4.3,
+    price: 34,
+    img: 'chat.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Palak Paneer',
+    category : 'south indian',
+    rating : 4.3,
+    price: 170,
+    img: 'palak.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Daal Tadak',
+    category : 'south indian',
+    rating : 4.3,
+    price: 110,
+    img: 'tadak.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Veg Shahi Korma',
+    category : 'south indian',
+    rating : 4.3,
+    price: 170,
+    img: 'shhahi.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Green Peas Masala',
+    category : 'south indian',
+    rating : 4.3,
+    price: 90,
+    img: 'green.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Veg Wanton Soup',
+    category : 'south indian',
+    rating : 4.3,
+    price: 70,
+    img: 'veg wanton soup.jpg',
+    quantity: 1
+},
+{
+id: 44,
+name: 'Sweet Corn Soup',
+category : 'south indian',
+rating : 4.3,
+price: 70,
+img: 'sweet.jpg',
+quantity: 1
+},
+{
+id: 44,
+name: 'Veg Manchow Soup',
+category : 'south indian',
+rating : 4.3,
+price: 70,
+img: 'manchow.jpg',
+quantity: 1
+},
+{
+    id: 44,
+    name: 'Tomato Soup',
+    category : 'south indian',
+    rating : 4.3,
+    price: 70,
+    img: 'tomato soup.jpg',
+    quantity: 1
+    },
+]
+
+export {foodItem};
